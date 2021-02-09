fa1fa9a57ca4053bec0324d021aad1b6c9053799
Federico <crudofederico@gmail.com>
2021-02-09 16:30:34 +0100

    update

--- js/scipt.js
+++ js/scipt.js
@@ -1,14 +1,36 @@
 var app = new Vue({
-    el: '.cds-container',
+    el: '#app',
     data: {
       dischi:[],
+      categorie:["all"],
+      selected:'',
+      search:"",
+    },
+
+    methods:{
+    //   searchContact(){
+    //         this.dischi.forEach(element => {
+    //         (!element.title.includes(this.search))? element.visibility=false : element.visibility=true;
+
+    //     });
+    //     console.log(this.dischi);
+        
+      
+    // } 
     },
     mounted(){
       axios
         .get('https://flynn.boolean.careers/exercises/api/array/music')
         .then((result) => {
            this.dischi = result.data.response;
-
+           console.log(this.dischi);
+            this.dischi.forEach(element => {
+                if(!this.categorie.includes(element.genre)){
+                    this.categorie.push(element.genre);
+                }
+            });
+           
+            
         })
         .catch(error => console.log('errore'));
     }
